6c4c1b36ec769ceb903ef2c82ed23d38e232090e
Sokol V <1470715@gmail.com>
2018-01-05 16:21:10 +0100

    pagination

--- source/js/app.js
+++ source/js/app.js
@@ -3,7 +3,17 @@ $(document).ready(function() {
 		e.preventDefault()
 		$('.nav-wrapper').toggleClass('open');
 	})
+	
 	$('.select-mobile-only').selectize();
+
 	let newsImgWidth = $('.figure-img').width();
 	$('.figure-descr').width(newsImgWidth);
+
+	$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+		event.preventDefault();
+		$(this).ekkoLightbox({
+			showArrows: true
+		});
+	});
 });
+
